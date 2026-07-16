cba96324cae203ca2d2634275fd944eeb5517d3c
Piyush Thaware <piyush@Piyushs-MacBook-Air.local>
2026-07-17 01:11:01 +0530

    Add : initial nodejs app

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const express = require("express");
+const morgan = require("morgan");
+const os = require("os");
+
+const app = express();
+const APP_PORT = process.env.APP_PORT || 3000;
+const APP_NAME = process.env.APP_NAME || "default";
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Root endpoint
+app.get("/", (req, res) => {
+  res.json({
+    message: "Node.js Server is running",
+    port: APP_PORT,
+    appName: APP_NAME,
+  });
+});
+
+// Host information endpoint
+app.get("/hostname", (req, res) => {
+  res.json({
+    hostname: os.hostname(),
+    platform: os.platform(),
+    architecture: os.arch(),
+    cpus: os.cpus().length,
+    totalMemoryGB: (os.totalmem() / 1024 / 1024 / 1024).toFixed(2),
+    freeMemoryGB: (os.freemem() / 1024 / 1024 / 1024).toFixed(2),
+    uptimeSeconds: os.uptime(),
+    nodeVersion: process.version,
+    pid: process.pid,
+  });
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`App-[${APP_NAME}] running on http://localhost:${APP_PORT}`);
+});
